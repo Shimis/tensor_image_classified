a15cb6d8d01b6e13dc949a65f2d4f4278322f646
Shimis <mr.shimis@gmail.com>
2024-07-15 21:13:00 +0300

    converter

---
+++ converter/converter.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Загрузите предобученную модель MobileNet V2 из TensorFlow Keras Applications
+mobilenet_v2_model = tf.keras.applications.MobileNetV2(weights='imagenet', input_shape=(224, 224, 3))
+
+# Преобразуйте модель в формат TensorFlow Lite
+converter = tf.lite.TFLiteConverter.from_keras_model(mobilenet_v2_model)
+tflite_model = converter.convert()
+
+# Сохраните модель в файл
+with open('mobilenet_v2.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+print("Model converted and saved as mobilenet_v2.tflite")
